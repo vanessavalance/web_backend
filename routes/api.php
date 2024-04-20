55378d0cb3ae37f20a5575dd9707cddb990926c8
vanessavalance <165674177+vanessavalance@users.noreply.github.com>
2024-04-20 15:16:00 +0700

    [Add] feature Logout User

--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,6 @@ Route::group(['prefix'=>'v1'],function(){
     Route::middleware(App\Http\Middleware\ApiAuthMiddleware::class)->group(function (){
         Route::get('/users/profile',[App\Http\Controllers\UserController::class,'get']);
         Route::patch('/users/profile',[App\Http\Controllers\UserController::class,'update']);
+        Route::delete('/users/logout',[App\Http\Controllers\UserController::class,'logout']);
     });
 });
