533a50a83a18891bcff9d59cd7f9c597a70c8f0f
vanessavalance <165674177+vanessavalance@users.noreply.github.com>
2024-04-20 15:02:42 +0700

    [Add] Feature Update User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Http\Resources\UserResource;
 use App\Models\Users;
 
@@ -43,6 +44,21 @@ class UserController extends Controller
     public function get(Request $request): UserResource
     {
         $user = Auth::user();
+        return new UserResource($user);
+    }
+
+    public function update(UserUpdateRequest $request): UserResource
+    {
+        $data = $request->validated();
+        $user = Auth::user();
+
+        if (isset($data['password'])) {
+            $user->password = Hash::make($data['password']);
+        }
+        if (isset($data['name'])) {
+            $user->name = $data['name'];
+        }
+        $user->save();
         return new UserResource($user);
     }
 }
